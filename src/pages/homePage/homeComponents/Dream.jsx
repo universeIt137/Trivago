db33ce147d6d5d196f522972643c8f1677aeee87
mehedi071967 <monirujjaman362munna@gmail.com>
2024-12-31 12:38:00 +0600

    some changes

--- src/pages/homePage/homeComponents/Dream.jsx
+++ src/pages/homePage/homeComponents/Dream.jsx
@@ -4,6 +4,8 @@ const Dream = () => {
     return (
         <div>
             This is Dream section
+            <img src="https://res.cloudinary.com/dqmmlxnee/image/upload/v1735623681/Trivago/ytvenvth1rfgsvte2mss.png" alt="" />
+          <img src="https://res.cloudinary.com/dqmmlxnee/image/upload/v1735623681/Trivago/rdxrg2hpnng7bfnpuoxq.png" alt="" />
         </div>
     );
 };
