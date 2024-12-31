78524fb7e3e6f188f107e8c1f3b2123ce7a77e11
Md Fahim <mdf17548@gmail.com>
2024-12-31 12:19:20 +0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <div>
-            This is Navbar
+        <div className='w-full'>
+            <nav className='bg-white '>
+                <div className="flex justify-between items-center">
+                <div className='font-bold text-3xl'>
+                   <NavLink to="/" className="logo">TRAVLOG</NavLink> 
+                </div>
+                
+                
+                    <ul>
+                        <li>
+                        <NavLink to="/" activeClassName="active">Home</NavLink>
+                        </li>
+                        <li>
+                        <NavLink to="/about" activeClassName="active">About</NavLink>
+                        </li>
+                        <li>
+                        <NavLink to="/destination" activeClassName="active">Destination</NavLink></li>   
+                        <li>
+                        <NavLink to="/blog" activeClassName="active">Blog</NavLink>
+                        </li>
+                        <li>
+                        <NavLink to="/package" activeClassName="active">Package</NavLink>
+                        </li>
+                        <li>
+                        <NavLink to="/contact" activeClassName="active">Contact</NavLink>
+                        </li>                 
+                    </ul>
+                
+                <div>
+                <ul>
+                        <li>
+                            <NavLink to="/login" activeClassName="active">Log In</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/signup" activeClassName="active">Sign Up</NavLink>
+                        </li>
+                    </ul>
+                </div>
+                </div>
+            </nav>
         </div>
     );
 };
