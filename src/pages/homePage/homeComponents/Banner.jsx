6f0eb5446d0b0c16e3d57b8e66b47fdfdec4d25d
shariful104 <sharifulpc04@gmail.com>
2024-12-31 12:59:32 +0600

    banner design

--- src/pages/homePage/homeComponents/Banner.jsx
+++ src/pages/homePage/homeComponents/Banner.jsx
@@ -4,12 +4,29 @@ import { IoBagHandleSharp } from 'react-icons/io5';
 const Banner = () => {
     return (
         <div>
-            <div className="container max-w-7xl mx-auto">
+            <div className="container w-10/12 mx-auto">
                 <div className="flex justify-between  bg-blue-100 aspect-[10/5]">
                     {/* text content side */}
-                    <div className="">
-                        <p className="font-semibold px-4 py-3 bg-white rounded-lg m-2 flex items-center">Explore the world!<IoBagHandleSharp />
+                    <div className="w-1/2">
+                        <div className="text-center flex items-center"><p className=" font-semibold flex items-center px-4 py-3 bg-white rounded-lg m-2 ">Explore the world!<IoBagHandleSharp />
                         </p>
+                        </div>
+
+                        <div className="mt-8">
+                            <p className="text-6xl font-bold p-2"><span className="">Travel <span className="text-pink-700">top</span></span></p>
+                            <p className="text-6xl font-bold p-2"><span className="text-pink-700">destination </span></p>
+                            <p className="text-6xl font-bold p-2"><span className="">of the world </span></p>    
+                        </div>
+
+                        <div className="mt-12">
+                            <p className="">Where advanture meet comport. We create</p>
+                            <p className="">unforgettable travel experinece.</p>
+                        </div>
+
+                        <div className="mt-12 space-x-2">
+                            <button className="px-6 py-2 bg-blue-400 rounded-lg"><a href="" className="">Get start</a></button>
+                            <button className="px-6 py-2 bg-blue-400 rounded-lg"><a href="" className="">Watch demo</a></button>
+                        </div>
                     </div>
 
 
