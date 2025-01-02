a4e1e77bc8837138d4ebd743b74c92e7d6400023
Md Fahim <mdf17548@gmail.com>
2025-01-02 11:05:48 +0600

    Navbar and some chances in destination

--- src/pages/homePage/homeComponents/TopDestination.jsx
+++ src/pages/homePage/homeComponents/TopDestination.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
-
-const TopDestination = () => {
+const Destination = () => {
     return (
         <div>
-            <div className="">
-                <p></p>
-            </div>
-            <div className="">
-                <div className=""></div>
-            </div>
+            <section className='container mx-auto w-10/12 mb-20'>
+                <div className='text-pink-600'>WHAT WE SERVE</div>
+                <div className='grid grid-cols-1 lg:grid-cols-3 gap-10 '>
+                    <div className='rounded-lg '>
+                        <img className='rounded-[40px] shadow-lg ' src="https://res.cloudinary.com/dntcuf8u3/image/upload/v1735624339/Trivago/ecoptyoaia64q1fsjost.png" alt="" />
+                        <p >Cappadocia</p>
+                    </div>
+                    <div>
+                        <img className='rounded-[40px] shadow-lg' src="https://res.cloudinary.com/dntcuf8u3/image/upload/v1735624339/Trivago/olyuocfddhautd5d3ece.png" alt="" />
+                    </div>
+                    <div>
+                        <img className='rounded-[40px] shadow-lg' src="https://res.cloudinary.com/dntcuf8u3/image/upload/v1735624340/Trivago/bd75rbsvucngprhyxv7i.png" alt="" />
+                    </div>
+                </div>
+            </section>
         </div>
     );
 };
