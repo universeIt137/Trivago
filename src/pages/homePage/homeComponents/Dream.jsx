b1ff77e0be1d460f7559085761af7787f54540df
shuvoagun <shuvoagun898@gmail.com>
2025-01-02 10:31:20 +0600

    dream

--- src/pages/homePage/homeComponents/Dream.jsx
+++ src/pages/homePage/homeComponents/Dream.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import  React from 'react';
 
 const Dream = () => {
     return (
@@ -7,8 +7,14 @@ const Dream = () => {
                 <img className='w-[570px]' src="https://res.cloudinary.com/da43e0ikj/image/upload/v1735624391/Trivago/dream%20img/fhot9gkj6a9hlwuywn6z.png" alt="" />
                 <img className='-mt-[440px] ml-24 w-[225px] ' src="https://res.cloudinary.com/da43e0ikj/image/upload/v1735624391/Trivago/dream%20img/lnxdt7yjktp4as0nwiy2.png" alt="" />
             </div>
-            <div className='ml-6'>
+            <div className='ml-24'>
                 <p className='text-2xl text-pink-300'>We Are The Best For You</p>
+                <p className='text-6xl'>Unlock Your Dream <br />
+                Destination</p>
+                <p className='text-xl mt-6'>We Are Dedicated To Making Your Journey Of <br /> Descovery Truly Unforgatteble.Our Team Of Passionate<br/>Travel Exparts Is Hero To Assist You In Finding The<br/> Destination Of Your Dream</p>
+            </div>
+            <div>
+                <p className='text-lg'>4k+</p>
                 <p></p>
             </div>
         </div>
